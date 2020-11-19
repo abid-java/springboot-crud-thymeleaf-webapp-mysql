dde8994199d442b1e46df7fbd131bed25fa0d65d
abmohamm <mohd.92java@gmail.com>
2020-11-19 16:37:06 -0500

    [US1234]:Created skeleton springboot-thymeleaf application

---
+++ springboot-crud-thymeleaf-webapp-mysql/src/main/java/com/app/samples/springboot/SpringbootCrudThymeleafWebappMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.app.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudThymeleafWebappMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudThymeleafWebappMysqlApplication.class, args);
+	}
+
+}
